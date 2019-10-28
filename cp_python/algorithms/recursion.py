0b99c6af573adf2e6396f411e8acd2dc8fd3f973
Matthäus Heer <maheer@student.ethz.ch>
2019-10-28 20:54:57 +0100

    Added BigO analysis for fibonacci variants.

--- cp_python/algorithms/recursion.py
+++ cp_python/algorithms/recursion.py
@@ -14,12 +14,22 @@ def factorial_iterative(n: int) -> int:
 
 
 def fibonacci(n: int) -> int:
+    """
+    Time:  O(2^n)  -> Since the recursive call tree has O(2^n) nodes representing calls and each call is O(1).
+                      Since the call tree is actually not balanced (the right side is always smaller then left), the
+                      actual runtime is O(1.6^n) but since BigO is an upper bound on runtime O(2^n) is still correct.
+    Space: O(logn) -> Since at any moment at most max_depth(call_tree) stack frames are on the stack which is O(logn).
+    """
     if n == 0 or n == 1:
         return n
     return fibonacci(n - 1) + fibonacci(n - 2)
 
 
 def fibonacci_memoization(n: int, memo: dict = None) -> int:
+    """
+    Time:  O(n)    -> By caching results of each call results we have at most n calls to process.
+    Space: O(n)    -> The memory requirement increases since we have the lookup table storing cached output.
+    """
     memo = memo if memo else {0: 1, 1: 1}  # Since we really, really have to avoid mutable default args :-)
     if n == 0 or n == 1:
         return n
