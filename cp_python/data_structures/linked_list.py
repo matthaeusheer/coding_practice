d8697079c9c70e238a719e4f67ed443a675f4dd2
Matthäus Heer <maheer@student.ethz.ch>
2019-10-28 20:54:11 +0100

    Implemented Linked List class incl. testing.

---
+++ cp_python/data_structures/linked_list.py
@@ -0,0 +1,84 @@
+class LinkedList:
+    def __init__(self, head=None):
+        self.head = head
+        self.size = 0
+
+    def prepend(self, data) -> None:
+        """Inserts an element at the beginning. Time: O(1)."""
+        self.head = Node(data, next_=self.head)
+        self.size += 1
+
+    def append(self, data) -> None:
+        """Inserts an element at the end. Time: O(n), n: size of linked list."""
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            self.size += 1
+            return
+        node = self.head
+        while node.next:
+            node = node.next
+        node.next = new_node
+        self.size += 1
+
+    def insert(self, data, position) -> None:
+        """Inserts an element at given index position. Time: O(n) worst case, n: size of linked list."""
+        assert position - 1 <= self.size, f'Cannot insert at position {position} for list of size {self.size}.'
+        if position == 0:
+            return self.prepend(data)
+        node = self.head
+        for idx in range(position - 1):
+            node = node.next
+        new_node = Node(data, next_=node.next)
+        node.next = new_node
+        self.size += 1
+
+    def remove(self, data) -> bool:
+        """Remove first occurrence of an item in the list. Time: O(n)."""
+        node = self.head
+        if node.data == data:  # hit on head
+            self.head = node.next if node.next else None
+            self.size -= 1
+            return True
+        while node.next:
+            if node.next.data == data:
+                node.next = node.next.next if node.next.next else None
+                self.size -= 1
+                return True
+            node = node.next
+        return False
+
+    def display(self):
+        """Prints out the whole linked list. Time: O(n) since we need to traverse the whole thing."""
+        print(self.to_list())
+
+    def search(self, data) -> int:
+        """Searches for an item and returns the index if found, else returns -1. Time: O(n)."""
+        if not self.head:
+            return -1  # not found
+        node = self.head
+        idx = 0
+        while node.next:
+            if node.data == data:
+                return idx
+            node = node.next
+            idx += 1
+        return -1
+
+    def to_list(self):
+        """Convert the linked list to a usual Python list. Time: O(n)."""
+        node = self.head
+        if self.size == 0:
+            return []
+        list_ = []
+        while node.next is not None:
+            list_.append(node.data)
+            node = node.next
+        list_.append(node.data)
+        return list_
+
+
+class Node:
+    def __init__(self, data, next_=None):
+        self.data = data
+        self.next = next_
---
+++ cp_python/tests/data_structures/test_linked_list.py
@@ -0,0 +1,76 @@
+import unittest
+from functools import partial
+from data_structures.linked_list import LinkedList
+
+
+class TestLinkedListCreation(unittest.TestCase):
+    def test_creation(self):
+        """Calling prepend, append and insert(0, data) on empty linked list."""
+        ll = LinkedList()
+        self.assertEqual([], ll.to_list()), 'Linked List should be empty.'
+        self.assertEqual(0, ll.size), 'Empty list should have size 0'
+        # prepend
+        ll.prepend(1)
+        self.assertEqual([1], ll.to_list()), 'LL should have exactly one element.'
+        self.assertEqual(1, ll.size), 'Should be size 1.'
+        # append
+        ll = LinkedList()
+        ll.append(1)
+        self.assertEqual([1], ll.to_list()), 'LL should have exactly one element.'
+        self.assertEqual(1, ll.size), 'Should be size 1.'
+        ll = LinkedList()
+        # insert at zero
+        ll.insert(1, 0)
+        self.assertEqual([1], ll.to_list()), 'LL should have exactly one element.'
+        self.assertEqual(1, ll.size), 'Should be size 1.'
+
+
+class TestLinkedListOperations(unittest.TestCase):
+    def setUp(self) -> None:
+        ll = LinkedList()
+        ll.prepend(5)
+        ll.prepend(4)
+        ll.prepend(3)
+        ll.prepend(2)
+        ll.prepend(1)
+        self.test_list = ll
+
+    def test_insert(self):
+        self.test_list.insert(10, 1)
+        self.assertEqual([1, 10, 2, 3, 4, 5], self.test_list.to_list())
+
+    def test_append(self):
+        self.test_list.append(10)
+        self.assertEqual([1, 2, 3, 4, 5, 10], self.test_list.to_list())
+
+    def test_prepend(self):
+        self.test_list.prepend(10)
+        self.assertEqual([10, 1, 2, 3, 4, 5], self.test_list.to_list())
+
+    def test_remove_return(self):
+        self.assertEqual(True, self.test_list.remove(3))
+        self.assertEqual(False, self.test_list.remove(10))
+
+    def test_remove_last(self):
+        self.test_list.remove(5)
+        self.assertEqual([1, 2, 3, 4], self.test_list.to_list())
+        self.assertEqual(4, self.test_list.size)
+
+    def test_remove_first(self):
+        self.test_list.remove(1)
+        self.assertEqual([2, 3, 4, 5], self.test_list.to_list())
+        self.assertEqual(4, self.test_list.size)
+
+    def test_remove(self):
+        self.test_list.remove(3)
+        self.assertEqual([1, 2, 4, 5], self.test_list.to_list())
+        self.assertEqual(4, self.test_list.size)
+
+    def test_search(self):
+        self.assertEqual(0, self.test_list.search(1))
+        self.assertEqual(-1, self.test_list.search(100))
+        self.assertEqual(2, self.test_list.search(3))
+
+    def test_search_empty(self):
+        ll = LinkedList()
+        self.assertEqual(-1, ll.search(1))
