d3a9e6c8fe1253af80dfefe09efccd7f29ce9e26
matthaeusheer <maheer@student.ethz.ch>
2019-11-09 16:31:45 +0100

    Added Stack with O(1) min value lookup.

--- cp_python/data_structures/stack.py
+++ cp_python/data_structures/stack.py
@@ -18,15 +18,22 @@ Complexity
 
 Implementation
 --------------
-    We use a LinkedList. The Stack stores a reference to the top element which is the head of the underlying
-    LinkedList.
+    Two possibilities
+        LinkedList. The Stack stores a reference to the top element which is the head of the underlying LinkedList.
+        Array. The Stack holds an array and the index of the top most (last) element. If the array is filled up,
+        a new, larger array is being created and old content is being copied over.
 """
-from data_structures.linked_list import LinkedList, Node
+from abc import ABC
+from data_structures.linked_list import Node
 
 
-class Stack:
+class Stack(ABC):
+    pass
+
+
+class LinkedListStack(Stack):
     def __init__(self):
-        self.top = LinkedList().head
+        self.top = None
 
     def pop(self):
         if not self.top:
@@ -45,6 +52,116 @@ class Stack:
 
     def is_empty(self):
         return self.top is None
+
+
+class ArrayStack(Stack):
+    def __init__(self):
+        self.curr_size = 1  # default size
+        self.arr = self.curr_size * []
+        self.top_idx = None
+
+    def pop(self):
+        if self.top_idx is None:
+            raise EmptyStackException
+        ret = self.peek()
+        self.top_idx = self.top_idx - 1 if self.top_idx > 0 else None
+        return ret
+
+    def peek(self):
+        if self.top_idx is None:
+            raise EmptyStackException
+        return self.arr[self.top_idx]
+
+    def push(self, item):
+        if self.top_idx is None:
+            self.top_idx = 0
+        else:
+            self.top_idx += 1
+        if self.top_idx == len(self.arr):
+            tmp = self.arr.copy()
+            self.curr_size *= 2
+            self.arr = self.curr_size * [None]
+            self.arr[0:len(tmp)] = tmp
+        self.arr[self.top_idx] = item
+
+    def is_empty(self):
+        return self.top_idx is None
+
+
+class NodeWithMin(Node):
+    def __init__(self, data, min_, next_=None):
+        super().__init__(data, next_)
+        self.min = min_
+
+
+class StackWithMin(Stack):
+    """How would you design a stack which, in addition to push and pop, has a function min which returns the minimum
+    element? Push, pop and min should all operate in 0(1) time!
+    Idea:
+            Suppose the stack has a minimum member variable and for every push, we check if the new element is smaller
+            than the current minimum of the stack. If so, we update the min variable. Problems arise when popping an
+            element of the stack. Which is the next smaller element now? For that we have to search the whole stack
+            which is O(n).
+            Solution: Every node carries a member which indicates the minimum of the current stack it is in. If the own
+            value is smaller, then minimum gets set to this value, if not, the minimum value from the previous top is
+            taken over. That way each node knows which is the smallest element for itself and all nodes deeper in the
+            stack.
+    Optimization:
+            The solution above stores one integer for sure in every node. We "could" do better by having a second
+            stack being responsible for the min values inside the stack. If nodes added afterwards are usually larger
+            than say the first one, end up storing much less.
+    """
+    def __init__(self):
+        self.top = None
+
+    def push(self, item):
+        if self.top is None:
+            self.top = NodeWithMin(data=item, min_=item, next_=None)
+        else:
+            self.top = NodeWithMin(data=item, min_=item if item < self.top.min else self.top.min, next_=self.top)
+
+    def pop(self):
+        if self.top is None:
+            raise EmptyStackException
+        ret = self.top.data
+        self.top = self.top.next
+        return ret
+
+    def peek(self):
+        if self.top is None:
+            raise EmptyStackException
+        return self.top.data
+
+    def is_empty(self):
+        return self.top is None
+
+    def get_min(self):
+        if self.top is None:
+            raise EmptyStackException
+        return self.top.min
+
+
+class StackWithMinOpt(LinkedListStack):
+    def __init__(self):
+        super().__init__()
+        self.min_stack = LinkedListStack()
+
+    def push(self, item):
+        if item < self.get_min():
+            self.min_stack.push(item)
+        super().push(item)
+
+    def pop(self):
+        ret = super().pop()
+        if ret == self.get_min():
+            self.min_stack.pop()
+        return ret
+
+    def get_min(self):
+        if self.top is None:
+            return float('inf')
+        else:
+            return self.min_stack.peek()
 
 
 class EmptyStackException(Exception):
--- cp_python/tests/data_structures/test_stack.py
+++ cp_python/tests/data_structures/test_stack.py
@@ -1,15 +1,38 @@
 import unittest
-from data_structures.stack import Stack
+from data_structures.stack import LinkedListStack
+from data_structures.stack import ArrayStack
+from data_structures.stack import StackWithMin, StackWithMinOpt
 from data_structures.stack import EmptyStackException
 
 
 class TestStack(unittest.TestCase):
-    def test_stack(self):
-        stack = Stack()
-        self.assertTrue(stack.is_empty())
-        stack.push(1)
-        self.assertFalse(stack.is_empty())
-        self.assertEqual(1, stack.peek())
-        self.assertEqual(1, stack.pop())
-        self.assertTrue(stack.is_empty())
-        self.assertRaises(EmptyStackException, stack.pop)
+    def test_stacks(self):
+        for stack_type in [LinkedListStack, ArrayStack, StackWithMin]:
+            stack = stack_type()
+            self.assertTrue(stack.is_empty())
+            stack.push(1)
+            self.assertFalse(stack.is_empty())
+            self.assertEqual(1, stack.peek())
+            self.assertEqual(1, stack.pop())
+            self.assertTrue(stack.is_empty())
+            self.assertRaises(EmptyStackException, stack.pop)
+
+    def test_stack_wih_min(self):
+        for stack_type in [StackWithMin, StackWithMinOpt]:
+            stack = stack_type()
+            self.assertRaises(EmptyStackException, stack.pop)
+            self.assertTrue(stack.is_empty())
+            stack.push(9)
+            self.assertEqual(9, stack.get_min())
+            stack.push(10)
+            self.assertEqual(9, stack.get_min())
+            stack.push(8)
+            self.assertEqual(8, stack.get_min())
+            self.assertFalse(stack.is_empty())
+            self.assertEqual(8, stack.peek())
+            self.assertEqual(8, stack.pop())
+            self.assertEqual(9, stack.get_min())
+            self.assertEqual(10, stack.pop())
+            self.assertEqual(9, stack.pop())
+            self.assertTrue(stack.is_empty())
+            self.assertRaises(EmptyStackException, stack.pop)
