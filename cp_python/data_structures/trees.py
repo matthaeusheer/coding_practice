7fec4cf0c2cd8420ba5cdfc9465af96584d00fe1
matthaeusheer <maheer@student.ethz.ch>
2019-11-14 23:32:01 +0100

    Added trees: Binary Tree and Binary Search Tree data structures.

---
+++ cp_python/data_structures/trees.py
@@ -0,0 +1,103 @@
+from typing import List, Generator, Callable
+
+
+#########################################
+#                                       #
+#   Generic Trees.                      #
+#                                       #
+#########################################
+
+class Node:
+    def __init__(self, data, children: List = None):
+        self.data = data
+        self.children = children
+
+
+#########################################
+#                                       #
+#   Binary Trees.                       #
+#                                       #
+#########################################
+
+class BinaryTreeNode:
+    def __init__(self, data, left=None, right=None) -> None:
+        self.data = data
+        self.left = left
+        self.right = right
+
+
+def bin_traverse_in_order(node: BinaryTreeNode) -> Generator:
+    """Traversal scheme: Traverse left, then current, then right."""
+    if node.left:
+        yield from bin_traverse_in_order(node.left)
+    yield node.data
+    if node.right:
+        yield from bin_traverse_in_order(node.right)
+
+
+def bin_traverse_pre_order(node: BinaryTreeNode) -> Generator:
+    """Traversal scheme: Traverse current, then left, then right."""
+    yield node.data
+    if node.left:
+        yield from bin_traverse_pre_order(node.left)
+    if node.right:
+        yield from bin_traverse_pre_order(node.right)
+
+
+def bin_traverse_post_order(node: BinaryTreeNode) -> Generator:
+    """Traversal scheme: Traverse left, then right, then current."""
+    if node.left:
+        yield from bin_traverse_post_order(node.left)
+    if node.right:
+        yield from bin_traverse_post_order(node.right)
+    yield node.data
+
+
+def print_bin_traversal(node: BinaryTreeNode, traverse_func: Callable) -> None:
+    print(f'Traversing with {traverse_func.__name__}...')
+    for idx, item in enumerate(traverse_func(node)):
+        print(f'{idx:>5}: {item}')
+    print()
+
+
+#########################################
+#                                       #
+#   Binary Search Trees.                #
+#                                       #
+#########################################
+
+class BinarySearchTreeNode(BinaryTreeNode):
+    def __init__(self, data, left=None, right=None) -> None:
+        super().__init__(data, left, right)
+
+    def insert(self, new_data) -> None:
+        if new_data <= self.data:
+            if not self.left:
+                self.left = BinarySearchTreeNode(new_data)
+            else:
+                self.left.insert(new_data)
+        else:
+            if not self.right:
+                self.right = BinarySearchTreeNode(new_data)
+            else:
+                self.right.insert(new_data)
+
+    def contains(self, lookup_data) -> bool:
+        if self.data == lookup_data:
+            return True
+        elif lookup_data < self.data:
+            if not self.left:
+                return False
+            else:
+                return self.left.contains(lookup_data)
+        else:
+            if not self.right:
+                return False
+            else:
+                return self.right.contains(lookup_data)
+
+
+
+    def print(self):
+        """In a binary search tree, by definition, in-order traverse will print the elements in order."""
+        pass
---
+++ cp_python/tests/data_structures/test_trees.py
@@ -0,0 +1,49 @@
+import unittest
+
+from data_structures import trees
+from tests.test_utils import run_sub_tests
+
+
+class TestTreeTraversal(unittest.TestCase):
+    def setUp(self) -> None:
+        child121 = trees.BinaryTreeNode(data=6)
+        child12 = trees.BinaryTreeNode(data=5, left=child121)
+        child11 = trees.BinaryTreeNode(data=4)
+        child1 = trees.BinaryTreeNode(data=2, left=child11, right=child12)
+        child2 = trees.BinaryTreeNode(data=3)
+        self.root = trees.BinaryTreeNode(data=1, left=child1, right=child2)
+
+    @unittest.SkipTest
+    def test_print_traversals(self) -> None:
+        """Helper function without testing functionality to see tree traversal in action."""
+        for func in [trees.bin_traverse_in_order, trees.bin_traverse_pre_order, trees.bin_traverse_post_order]:
+            trees.print_bin_traversal(self.root, func)
+    
+    def test_bin_traversal_in_order(self):
+        self.assertListEqual([4, 2, 6, 5, 1, 3], list(trees.bin_traverse_in_order(self.root)))
+
+    def test_bin_traversal_pre_order(self):
+        self.assertListEqual([1, 2, 4, 5, 6, 3], list(trees.bin_traverse_pre_order(self.root)))
+
+    def test_bin_traversal_post_order(self):
+        self.assertListEqual([4, 6, 5, 2, 3, 1], list(trees.bin_traverse_post_order(self.root)))
+
+
+class TestBinarySearchTree(unittest.TestCase):
+    def test_insert(self):
+        root = trees.BinarySearchTreeNode(1)
+        root.insert(5)
+        root.insert(10)
+        self.assertSetEqual({1, 5, 10}, set(trees.bin_traverse_in_order(root)))
+
+    def test_contains(self):
+        bst = trees.BinarySearchTreeNode(10)
+        for value in [15, 20, 25, 30, 5, 8, 3]:
+            bst.insert(value)
+        self.assertTrue(bst.contains(10))
+        self.assertTrue(bst.contains(15))
+        self.assertTrue(bst.contains(30))
+        self.assertFalse(bst.contains(1))
+        self.assertFalse(bst.contains(11))
+        self.assertFalse(bst.contains(24))
+
